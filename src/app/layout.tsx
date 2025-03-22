0884d2959b4e444da5a27fe159a468e609c60817
Danielmeyer007 <danielmeyer007@gmail.com>
2025-03-21 22:56:18 -0500

    new files added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,20 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
+        >
+          <source src="/videos/blackholewallpaper.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="nebula"></div>
+        <div className="gas-cloud"></div>
+        <div className="gas-cloud"></div>
+        <div className="gas-cloud"></div>
         {children}
       </body>
     </html>
